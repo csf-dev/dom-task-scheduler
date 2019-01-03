9eed6b2be70a8314863805897ee72865d7a10e7c
Craig Fowler <craig@csf-dev.com>
2019-01-03 21:17:22 +0000

    Greatly simplify the task scheduler
    It previously had a lot of logic to attempt to provide thread-safety
    by maintaining a collection of queues.  Of course - duh - JS is
    single-threaded by design and so that wasn't needed at all.
    
    Also, there is no benefit in keeping a reference to any promises
    which are used to trigger the execution of the queue.  We don't
    offer any functionality to abort the queue.

--- src/DomTaskScheduler.js
+++ src/DomTaskScheduler.js
@@ -7,41 +7,34 @@ import type { DomTask } from './SchedulesDomTasks';
 
 export class DomTaskScheduler implements SchedulesDomTasks {
     #domReadyFunction : DomReadyFunction;
-    #queues : Array<TaskQueue>;
-    #domReadyPromise : ?Promise<void>;
+    #queue : ?TaskQueue;
 
-    get hasTasks() : bool { return this.#queues.length > 0; }
+    get hasTasks() : bool { return this.#queue !== null; }
     get tasksComplete() : Promise<void> {
-        if(!this.hasTasks) { return Promise.resolve(); }
-        const allDone = this.#queues.map(queue => queue.complete);
-        return Promise.all(allDone).then(() => {});
+        if(!this.#queue) return Promise.resolve();
+        return this.#queue.complete;
     };
 
     addTask(task : DomTask) : void {
-        if(!this.hasTasks || !this.#domReadyFunction) {
-            const promiseFactory = this.#domReadyFunction;
-            this.#domReadyPromise = promiseFactory()
-                .then(() => this.runAllNow());
-            this.#queues.push(new TaskQueue());
+        if(!this.#queue) {
+            const getReadyPromise = this.#domReadyFunction;
+            getReadyPromise().then(() => this.runAllNow());
+            this.#queue = new TaskQueue();
         }
 
-        const lastQueue = this.#queues[this.#queues.length - 1];
-        lastQueue.add(task);
+        this.#queue.add(task);
     }
-    async runAllNow() : Promise<void> {
-        const queues = this.#queues.slice();
+    runAllNow() : Promise<void> {
+        const queue = this.#queue;
+        if(!queue) return Promise.resolve();
 
-        this.#domReadyPromise = null;
-
-        const results = queues.map(queue => queue.runAll());
-        await Promise.all(results);
-
-        this.#queues.splice(0, queues.length);
+        this.#queue = null;
+        return queue.runAll();
     }
 
     constructor(domReadyFunction : DomReadyFunction) {
         this.#domReadyFunction = domReadyFunction;
-        this.#queues = [];
+        this.#queue = null;
     }
 }
 
